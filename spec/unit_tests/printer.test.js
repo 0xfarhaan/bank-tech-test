c87381bbf7f5d2747c865a983b744c3f1a8701a6
farhaan-ali <fa8g12@gmail.com>
2020-11-02 21:27:23 +0000

    testing for multiple transactions being printed as opposed to just one

--- spec/unit_tests/printer.test.js
+++ spec/unit_tests/printer.test.js
@@ -40,3 +40,22 @@ test("Prints a statement with one debit transaction", () => {
     "date || credit || debit || balance\n02/11/2020 || || 12.00 || 10.00\n"
   );
 });
+
+test("Prints a statement with one credit and one debit transaction", () => {
+  console.log = jest.fn();
+  printer.print([
+    {
+      date: "02/11/2020",
+      credit: (10.0).toFixed(2),
+      balance: (10.0).toFixed(2),
+    },
+    {
+      date: "02/11/2020",
+      debit: (12.0).toFixed(2),
+      balance: (10.0).toFixed(2),
+    },
+  ]);
+  expect(console.log).toHaveBeenCalledWith(
+    "date || credit || debit || balance\n02/11/2020 || 10.00 || || 10.00\n02/11/2020 || || 12.00 || 10.00\n"
+  );
+});
