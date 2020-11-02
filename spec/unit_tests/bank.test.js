783c05dfea3fe761323b3d24fe211a779407de21
farhaan-ali <fa8g12@gmail.com>
2020-11-02 16:57:48 +0000

    printing out empty statement:
    added code to print out empty statement if no transactions exist
    unit test added to test behavior of function

--- spec/unit_tests/bank.test.js
+++ spec/unit_tests/bank.test.js
@@ -8,3 +8,9 @@ beforeEach(() => {
 test("expects account balance to start at zero", () => {
   expect(bank.balance).toStrictEqual(0);
 });
+
+test("expect an account with no transactions to return a empty statement", () => {
+  expect(bank.printStatement()).toStrictEqual(
+    "date || credit || debit || balance"
+  );
+});
--- src/bank.js
+++ src/bank.js
@@ -1,6 +1,11 @@
 class Bank {
   constructor() {
     this.balance = 0;
+    this._header = "date || credit || debit || balance";
+  }
+
+  printStatement() {
+    return this._header;
   }
 }
 module.exports = Bank;
