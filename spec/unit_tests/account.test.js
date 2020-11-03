a248691e963dea45c3ce0f86b892bd7abda09889
farhaan-ali <fa8g12@gmail.com>
2020-11-03 08:33:56 +0000

    mock date to be fixed

--- spec/unit_tests/account.test.js
+++ spec/unit_tests/account.test.js
@@ -1,14 +1,19 @@
 let Account = require("../../src/account");
 let account;
 
+const globalDate = Date;
+
 beforeEach(() => {
   account = new Account();
+  const mockDate = new Date("02 Nov 2020");
+  global.Date = jest.fn().mockImplementation(() => mockDate); // mock Date "new" constructor
+  global.Date.now = jest.fn().mockReturnValue(mockDate.valueOf()); // mock Date.now
 });
 
 test("records a credit transaction", () => {
   account.transaction(100);
   expect(account.transactions[0]).toStrictEqual({
-    date: "03/11/2020",
+    date: "02/11/2020",
     credit: 100,
     balance: 100,
   });
@@ -17,7 +22,7 @@ test("records a credit transaction", () => {
 test("records a debit transaction", () => {
   account.transaction(-100);
   expect(account.transactions[0]).toStrictEqual({
-    date: "03/11/2020",
+    date: "02/11/2020",
     debit: 100,
     balance: -100,
   });
