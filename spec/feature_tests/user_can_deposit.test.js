6de637913daaba4ee763f693f7927d674ec47a9d
farhaan-ali <fa8g12@gmail.com>
2020-11-03 14:07:03 +0000

    added feature test
    test for deposit , withdraw and seeing statement

---
+++ spec/feature_tests/user_can_deposit.test.js
@@ -0,0 +1,28 @@
+let Bank = require("../../src/bank");
+let bank;
+
+beforeEach(() => {
+  bank = new Bank();
+  const mockDate = new Date("02 Nov 2020");
+  global.Date = jest.fn().mockImplementation(() => mockDate); // mock Date "new" constructor
+  global.Date.now = jest.fn().mockReturnValue(mockDate.valueOf()); // mock Date.now
+});
+
+test("User can deposit money and see it on their statement", () => {
+  bank.deposit(100);
+  console.log = jest.fn();
+  bank.printStatement();
+  expect(console.log).toHaveBeenCalledWith(
+    "date || credit || debit || balance\n02/11/2020 || 100 || || 100\n"
+  );
+});
+
+test("User can deposit and withdraw money and see it on their statement", () => {
+  bank.deposit(100);
+  bank.withdraw(50);
+  console.log = jest.fn();
+  bank.printStatement();
+  expect(console.log).toHaveBeenCalledWith(
+    "date || credit || debit || balance\n02/11/2020 || || 50 || 50\n02/11/2020 || 100 || || 100\n"
+  );
+});
