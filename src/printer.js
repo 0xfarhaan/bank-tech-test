5d0625b2b28772291a87b42cfee1fb6ea217f33b
farhaan-ali <fa8g12@gmail.com>
2020-11-04 11:45:24 +0000

    refactor _formatTransactions in printer
    add two helper methods to format creidt and debit transactions

--- src/printer.js
+++ src/printer.js
@@ -7,42 +7,26 @@ class Printer {
   print(transactions) {
     console.log(this._formatTransactions(transactions));
   }
-
+  // prettier-ignore
   _formatTransactions(transactions) {
     let toPrint = this._header;
     for (let i = transactions.length - 1; i >= 0; i--) {
       const element = transactions[i];
       if (element.credit) {
-        toPrint +=
-          element.date +
-          " " +
-          this._divider +
-          " " +
-          element.credit +
-          " " +
-          this._divider +
-          " " +
-          this._divider +
-          " " +
-          element.balance +
-          "\n";
+        toPrint += this._formatCreditLine([element.date, element.credit, element.balance,]);
       } else {
-        toPrint +=
-          element.date +
-          " " +
-          this._divider +
-          " " +
-          this._divider +
-          " " +
-          element.debit +
-          " " +
-          this._divider +
-          " " +
-          element.balance +
-          "\n";
+        toPrint += this._formatDebitLine([element.date, element.debit, element.balance,]);
       }
     }
     return toPrint;
+  }
+  // prettier-ignore
+  _formatCreditLine(line) {
+    return (line[0] + " " + this._divider + " " + line[1] + " " + this._divider + " " + this._divider+ " " + line[2] + "\n");
+  }
+  // prettier-ignore
+  _formatDebitLine(line) {
+    return (line[0] + " " + this._divider + " " + this._divider + " " + line[1] + " " + this._divider + " " + line[2] + "\n");
   }
 }
 
